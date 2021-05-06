7fa0838096918c023b343c57b88d96873250d057
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-06 12:06:50 +0100

    Added registration + security

---
+++ src/main/java/com/loginpagedemowithspring/demo/Registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package com.loginpagedemowithspring.demo.Registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
