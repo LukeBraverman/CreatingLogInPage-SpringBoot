26273d9678c584baa8c8de6a117707219655f5f0
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-06 15:33:37 +0100

    Registration Service added

--- src/main/java/com/loginpagedemowithspring/demo/Registration/RegistrationController.java
+++ src/main/java/com/loginpagedemowithspring/demo/Registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.loginpagedemowithspring.demo.Registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class RegistrationController {
     private  RegistrationService registrationService;
-
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
--- src/main/java/com/loginpagedemowithspring/demo/Registration/RegistrationService.java
+++ src/main/java/com/loginpagedemowithspring/demo/Registration/RegistrationService.java
@@ -1,10 +1,28 @@
 package com.loginpagedemowithspring.demo.Registration;
 
+import com.loginpagedemowithspring.demo.appuser.AppUser;
+import com.loginpagedemowithspring.demo.appuser.AppUserRole;
+import com.loginpagedemowithspring.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
